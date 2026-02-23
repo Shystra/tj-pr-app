4eac38133400e094f88596e4c638a0c5b3b92eae
Shystra <jlsp.2019@hotmail.com>
2026-02-23 16:03:45 -0300

    feat: add tela de controle.

--- tj-pr-app/src/app/features/control/control.component.ts
+++ tj-pr-app/src/app/features/control/control.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -10,6 +11,7 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     TranslateModule,
     CommonModule,
+    ReactiveFormsModule,
     MatIconModule
   ],
   templateUrl: './control.component.html',
@@ -17,5 +19,41 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class ControlComponent {
 
+  form: FormGroup;
+  fotoPreview: string | null = null;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: ['', Validators.required],
+      rg: [''],
+      oab: [''],
+      uf: [''],
+      telefone: [''],
+      email: [''],
+      setor: [''],
+      numeroCartao: ['', Validators.required],
+      tipoCartao: [''],
+      tipoMovimentacao: [''],
+      loginRede: [''],
+      numeroSeguranca: [''],
+      destino: [''],
+      observacoes: ['']
+    });
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) return;
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.fotoPreview = reader.result as string;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
